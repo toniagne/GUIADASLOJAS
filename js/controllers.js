61a268ccc3ac8152a32a7517d682aa580ec3c08c
toniagne <toniagne@gmail.com>
2016-06-03 16:33:51 -0300

    INICIAL

---
+++ js/controllers.js
@@ -0,0 +1,18 @@
+angular.module('app.controllers', [])
+  
+.controller('lOGOLOJASCtrl', function($scope) {
+
+})
+   
+.controller('cATEGORIASCtrl', function($scope) {
+
+})
+   
+.controller('cATEGORIASELECIONADACtrl', function($scope) {
+
+})
+   
+.controller('cLIENTESELECIONADOCtrl', function($scope) {
+
+})
+    
